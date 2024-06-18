157341cfa7cf82c91934d1eb853ed83487972758
Kl <kir@>
2024-06-18 23:55:19 +0300

    Initial Commit

---
+++ src/main/java/com/boiler/weatherwatcher2/WeatherWatcher2Application.java
@@ -0,0 +1,13 @@
+package com.boiler.weatherwatcher2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWatcher2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherWatcher2Application.class, args);
+    }
+
+}
